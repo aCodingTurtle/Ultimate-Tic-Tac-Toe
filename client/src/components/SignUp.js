bd4dc9813036c8feaf1a349a9f03f2f52b7a323d
aCodingTurtle <34428554+aCodingTurtle@users.noreply.github.com>
2023-10-28 12:01:27 -0400

    Started over and switched tutorials

---
+++ client/src/components/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SignUp() {
+  return (
+    <div className='signUp'>
+        <label>Sign Up</label>
+        <input placeholder='Username'></input>
+    </div>
+  )
+}
+
+export default SignUp
